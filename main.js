089ba349af892cb9356d57f62eb45085357a95e8
Robert-T-Turnbull <rturnbull08@gmail.com>
2021-05-09 17:32:03 -0400

    Basic test

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+})
+    
+client.on("message", message => {
+    if (message.content === "ping") {
+        let reply = "hello";
+        message.reply(reply);
+    }
+})
+    
+client.login(process.env.TOKEN);
