457afd39ad0e541ef723d4d0fbef8c089e316fa7
Robert-T-Turnbull <rturnbull08@gmail.com>
2021-05-09 17:46:57 -0400

    Fixed spacing
    Made indentation 4 spaces

--- main.js
+++ main.js
@@ -3,13 +3,13 @@ const client = new Discord.Client();
 
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`);
-  })
+});
   
-  client.on("message", msg => {
+client.on("message", msg => {
     if (msg.content === "ping") {
-      let reply = "hello again"
-      msg.reply(reply);
+        let reply = "hello again"
+        msg.reply(reply);
     }
-  })
+});
   
-  client.login(process.env.TOKEN);
+client.login(process.env.TOKEN);
