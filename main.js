cdea876f47063f09a762a51fb6b458c40b8f2df6
Robert-T-Turnbull <rturnbull08@gmail.com>
2021-05-09 20:52:49 -0400

    Created prefix command for bot
    Created prefix for bot to only interpret certain messages

--- main.js
+++ main.js
@@ -2,13 +2,17 @@ const Discord = require("discord.js");
 const configuration = require('./configuration');
 const client = new Discord.Client();
 
+const PREFIX = "%";
+
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
   
-client.on("message", msg => {
-    input = msg.content;
-    parseInput(msg, input);
+client.on("message", message => {
+    if (message.author.bot) return;
+    if (message.content.startsWith(PREFIX)) {
+        message.reply("Hello there");
+    }
 });
 
 function parseInput (msg, input) {
