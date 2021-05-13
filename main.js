a2baff789822438db34670c62c63d6f870b7e902
Ryan Denniston <ryan.denniston@mail.utoronto.ca>
2021-05-13 00:43:16 -0400

    fixing regex for matching time ranges

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ const WEEKDAYS = ["Sunday", "Monday", "Tuesday", "Wednesday",
                 "Thursday", "Friday", "Saturday"]
 const MONTHS = ["January", "February", "March", "April", "May", "June",
                 "July", "August", "September", "October", "November", "December"]
+const timeRegex = new RegExp('^([0-1]?[0-9]|2[0-3]):[0-5][0-9](AM|PM)-([0-1]?[0-9]|2[0-3]):[0-5][0-9](AM|PM)$')
 
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -33,7 +34,9 @@ client.on("message", msg => {
 function addEvent (message) {
     let terms = message.content.split(" ")
     if (WEEKDAYS.includes(terms[1])) {
-        message.reply('Event on ' + terms[1])
+        if (timeRegex.test(terms[2])) {
+            message.reply('Recurring event on ' + terms[1] + ' at ' + terms[2]);
+        }
     } else if (MONTHS.includes(terms[1])) {
 
     } 
